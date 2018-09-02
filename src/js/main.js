6a7c6f0d6c92513cfd5b7adb2ee3572e6a2cce89
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2018-09-02 09:17:09 -0300

    Add babel

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 (function () {
-  var doc = document.documentElement;
+  const doc = document.documentElement;
 
   doc.classList.remove('no-js');
   doc.classList.add('js');
+
+  let greetings = 'Hello world!';
+  document.querySelector('h1').innerHTML = greetings;
 }());
